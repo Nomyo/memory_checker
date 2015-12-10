c04057f003b726e0e3c80dc858c32d59eb6765c1
Ashkan Kiaie Sandjie <kiaies_a@epita.fr>
2015-12-10 22:05:50 +0100

    move : get_r_debug_ardd in track.hh header

--- mem_strace_hook/strace_hook.cc
+++ mem_strace_hook/strace_hook.cc
@@ -6,21 +6,23 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <link.h>
-
+#include "track.hh"
+/*
 int in_child(char **argv)
 {
   ptrace(PTRACE_TRACEME, 0, 0, 0);
   return execvp(argv[0], argv);
 }
-
+*/
+/*
 struct r_debug *get_r_debug_addr(pid_t child)
 {
   //int status;
   child = child;
-  /*  waitpid(child, &status, 0);
+    waitpid(child, &status, 0);
   ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD
-  | PTRACE_O_TRACEFORK | PTRACE_O_TRACEVFORK | PTRACE_O_TRACECLONE);*/
-  ElfW(Phdr) *phdr = reinterpret_cast<ElfW(Phdr) *>(getauxval(AT_PHDR)); /* to fix */
+  | PTRACE_O_TRACEFORK | PTRACE_O_TRACEVFORK | PTRACE_O_TRACECLONE);
+  ElfW(Phdr) *phdr = reinterpret_cast<ElfW(Phdr) *>(getauxval(AT_PHDR));  to fix
   unsigned nb_phdr = getauxval(AT_PHNUM);
   for (unsigned i = 0; i < nb_phdr; i++, phdr++)
     {
@@ -37,7 +39,7 @@ struct r_debug *get_r_debug_addr(pid_t child)
       printf("version = %d\n", r_debug->r_version);
   return r_debug;
 }
-
+*/
 int main(int argc, char *argv[])
 {
   if (argc < 2)
@@ -48,9 +50,12 @@ int main(int argc, char *argv[])
   pid_t child = fork();
   argv++;
   if (child == 0)
-    return in_child(argv);
-
-  struct r_debug* r_debug = get_r_debug_addr(child);
-  r_debug = r_debug;
+    {
+      ptrace(PTRACE_TRACEME, 0, 0, 0);
+      return execvp(argv[0], argv);
+    }
+  struct Track::tracker tr;
+  tr.child = child;
+  tr.r_debug = Track::get_r_debug_addr(child); /* fill r_debug */
   return 0;
 }
