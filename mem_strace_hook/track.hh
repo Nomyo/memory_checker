eb27575c2e2ae2b94323be290f0698499e46fa95
Ashkan Kiaie Sandjie <kiaies_a@epita.fr>
2015-12-11 14:17:07 +0100

    fix : In track.hh, in struct tracker, functions that expected child pid as arguement, take now the attribute child

--- mem_strace_hook/track.hh
+++ mem_strace_hook/track.hh
@@ -5,6 +5,7 @@
 #include <elf.h>
 #include <link.h>
 #include <sys/auxv.h>
+#include <sys/uio.h>
 
 namespace Track
 {
@@ -13,11 +14,12 @@ namespace Track
     pid_t child;
     ElfW(Phdr) *phdr = NULL;
     unsigned long phnum = 0;
+    unsigned long phent = 0;
     struct r_debug *r_debug = NULL; /* map that stores the break point and library name */
     std::map<std::map<void *, unsigned long>, std::string> map;
-    void get_phdr(pid_t child);
+    void get_phdr();
     void get_r_debug_addr();
-    int init_tracker(pid_t child);
+    int init_tracker();
   };
 }
 #endif /* !TRACK_HH */
