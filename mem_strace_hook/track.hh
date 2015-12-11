485bd627e2a97d5f03d0d422fee3193d7743cf4e
Ashkan Kiaie Sandjie <kiaies_a@epita.fr>
2015-12-11 14:15:28 +0100

    fix : In track.hh map attribute becomes a map with key map and value are std::string

--- mem_strace_hook/track.hh
+++ mem_strace_hook/track.hh
@@ -1,36 +1,23 @@
+#ifndef TRACK_HH
+# define TRACK_HH
+
 #include <map>
+#include <elf.h>
+#include <link.h>
+#include <sys/auxv.h>
 
 namespace Track
 {
   struct tracker
   {
     pid_t child;
-    struct r_debug *r_debug;
-    std::map<void *, unsigned long> map; /* map that stores the break point */
+    ElfW(Phdr) *phdr = NULL;
+    unsigned long phnum = 0;
+    struct r_debug *r_debug = NULL; /* map that stores the break point and library name */
+    std::map<std::map<void *, unsigned long>, std::string> map;
+    void get_phdr(pid_t child);
+    void get_r_debug_addr();
+    int init_tracker(pid_t child);
   };
-
-  struct r_debug *get_r_debug_addr(pid_t child)
-  {
-    //int status;
-    child = child;
-    /*  waitpid(child, &status, 0);
-  ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD
-  | PTRACE_O_TRACEFORK | PTRACE_O_TRACEVFORK | PTRACE_O_TRACECLONE);*/
-    ElfW(Phdr) *phdr = reinterpret_cast<ElfW(Phdr) *>(getauxval(AT_PHDR)); /* to fix */
-    unsigned nb_phdr = getauxval(AT_PHNUM);
-    for (unsigned i = 0; i < nb_phdr; i++, phdr++)
-      {
-        if (phdr->p_type == PT_DYNAMIC)
-          {
-            printf("a_phdr = 0x%x\n", (unsigned int)phdr->p_vaddr);
-            break;
-          }
-      }
-    ElfW(Dyn) *dynamic = reinterpret_cast<ElfW(Dyn) *>(phdr->p_vaddr);
-    struct r_debug *r_debug = NULL;
-    for (; dynamic->d_tag != DT_DEBUG; dynamic++);
-    r_debug = reinterpret_cast<struct r_debug *>(dynamic->d_un.d_ptr);
-    printf("version = %d\n", r_debug->r_version);
-    return r_debug;
-  }
 }
+#endif /* !TRACK_HH */
