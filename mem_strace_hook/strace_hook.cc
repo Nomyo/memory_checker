24b7d05811f616e7783b2c79c51b2cf4bb8cce91
Ashkan Kiaie Sandjie <kiaies_a@epita.fr>
2015-12-12 02:58:17 +0100

    fix : loop to set r_debug

--- mem_strace_hook/strace_hook.cc
+++ mem_strace_hook/strace_hook.cc
@@ -7,39 +7,9 @@
 #include <sys/wait.h>
 #include <link.h>
 #include "track.hh"
-/*
-int in_child(char **argv)
-{
-  ptrace(PTRACE_TRACEME, 0, 0, 0);
-  return execvp(argv[0], argv);
-}
-*/
-/*
-struct r_debug *get_r_debug_addr(pid_t child)
-{
-  //int status;
-  child = child;
-    waitpid(child, &status, 0);
-  ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD
-  | PTRACE_O_TRACEFORK | PTRACE_O_TRACEVFORK | PTRACE_O_TRACECLONE);
-  ElfW(Phdr) *phdr = reinterpret_cast<ElfW(Phdr) *>(getauxval(AT_PHDR));  to fix
-  unsigned nb_phdr = getauxval(AT_PHNUM);
-  for (unsigned i = 0; i < nb_phdr; i++, phdr++)
-    {
-      if (phdr->p_type == PT_DYNAMIC)
-        {
-          printf("a_phdr = 0x%x\n", (unsigned int)phdr->p_vaddr);
-          break;
-        }
-    }
-  ElfW(Dyn) *dynamic = reinterpret_cast<ElfW(Dyn) *>(phdr->p_vaddr);
-  struct r_debug *r_debug = NULL;
-  for (; dynamic->d_tag != DT_DEBUG; dynamic++);
-        r_debug = reinterpret_cast<struct r_debug *>(dynamic->d_un.d_ptr);
-      printf("version = %d\n", r_debug->r_version);
-  return r_debug;
-}
-*/
+
+extern ElfW(Dyn) _DYNAMIC[];
+
 int main(int argc, char *argv[])
 {
   if (argc < 2)
@@ -55,7 +25,20 @@ int main(int argc, char *argv[])
       return execvp(argv[0], argv);
     }
   struct Track::tracker tr;
+  int status;
+  waitpid(child, &status, 0);
+  ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
   tr.child = child;
-  tr.r_debug = Track::get_r_debug_addr(child); /* fill r_debug */
+  tr.init_tracker();
+  while (true)
+    {
+      ptrace(PTRACE_SINGLESTEP, child, 0, 0);
+      waitpid(child, &status, 0);
+      if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
+        return 0;
+      if (WIFEXITED(status))
+        return 1;
+    }
+
   return 0;
 }
