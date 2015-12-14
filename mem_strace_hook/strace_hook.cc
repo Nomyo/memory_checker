92cbf79f90d5aedf449f67b64b1dc1ebe465cc54
Ashkan Kiaie Sandjie <kiaies_a@epita.fr>
2015-12-14 23:11:38 +0100

    fix : exit of the program

--- mem_strace_hook/strace_hook.cc
+++ mem_strace_hook/strace_hook.cc
@@ -15,33 +15,26 @@
 #include <sys/reg.h>
 #include <sys/user.h>
 
-extern ElfW(Dyn) _DYNAMIC[];
-
 void set_break(void *addr, pid_t pid)
 {
-  printf("breakpoint add at : 0x%lx\n", (unsigned long)addr);
   unsigned long ins = ptrace(PTRACE_PEEKDATA, pid, addr);
-
-  printf("instruction PRE %lx\n", ins);
   ptrace(PTRACE_POKEDATA, pid, addr, (ins & 0xffffffffffffff00) | 0xcc);
-  unsigned long ins2 = ptrace(PTRACE_PEEKDATA, pid, addr);
-  printf("instruction POST %lx\n", ins2);
 }
 
 int main(int argc, char *argv[])
 {
   if (argc < 2)
-    {
-      std::cerr << "usage: " << argv[0] << " arg\n";
-      exit(1);
-    }
+  {
+    std::cerr << "usage: " << argv[0] << " arg\n";
+    exit(1);
+  }
   pid_t child = fork();
   argv++;
   if (child == 0)
-    {
-      ptrace(PTRACE_TRACEME, 0, 0, 0);
-      return execvp(argv[0], argv);
-    }
+  {
+    ptrace(PTRACE_TRACEME, 0, 0, 0);
+    return execvp(argv[0], argv);
+  }
   struct Track::tracker tr;
   tr.r_debug = (struct r_debug *)malloc(sizeof (struct r_debug));
   int status;
@@ -49,39 +42,40 @@ int main(int argc, char *argv[])
   ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
   tr.child = child;
   tr.init_tracker(); /* From here r_debug is set */
-  Break l_break(child, tr.r_debug->r_state);
+  Break l_break(child, tr.r_debug->r_state, argv[0]);
   set_break((void *)tr.r_debug->r_brk, tr.child);
-
+  l_break.init_break();
   /* should set breakpoints in our own .init .text */
-
+  
   struct user_regs_struct regs;
   while (true)
+  {
+    ptrace(PTRACE_CONT, child, 0, 0);
+    waitpid(child, &status, 0);
+    if (WIFEXITED(status))
     {
-      ptrace(PTRACE_CONT, child, 0, 0);
-      waitpid(child, &status, 0);
-      std::cout << "STATUS = " << status << std::endl;
-      if (WIFEXITED(status))
-        {
-          l_break.print_breaks();
-          free(tr.r_debug);
-          return 0;
-        }
-      tr.get_r_debug_addr(); /* update r_debug */
-      if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) /* Program breaks */
-        {
-          ptrace(PTRACE_GETREGS, child, NULL, &regs);
-          printf("RIP 0x%lx\n", (unsigned long)regs.rip - 1);  /* state changed */
-          if ((unsigned long )regs.rip - 1 == (unsigned long)tr.r_debug->r_brk)
-            {
-              if (tr.r_debug->r_state == r_debug::RT_CONSISTENT) /* add or del lib*/
-                l_break.update(tr.r_debug->r_map); // update breakpoints
-              else
-                l_break.set_state(tr.r_debug->r_state); /* get previous state */
-            }
-          else
-            std::cout << "treat break and put the syscall back\n"; //l_break.rem_break(regs.rip, );
-        }
+      free(tr.r_debug);
+      printf("++++++ exited with %d ++++++\n", WIFEXITED(status));
+      return 0;
     }
-  /* FREE tr.r_debug */
+    tr.get_r_debug_addr(); /* update r_debug */
+    if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) /* Program breaks */
+    {
+      ptrace(PTRACE_GETREGS, child, NULL, &regs);
+      if ((unsigned long )regs.rip - 1 == (unsigned long)tr.r_debug->r_brk)
+      {
+        if (tr.r_debug->r_state == r_debug::RT_CONSISTENT) /* add or del lib*/
+          l_break.update(tr.r_debug->r_map); // update breakpoints
+        else
+          l_break.set_state(tr.r_debug->r_state); /* get previous state */
+      }
+      else
+      {
+        if (l_break.treat_break(tr.r_debug->r_map, regs.rip - 1, regs) == 1)
+          break;
+      }
+    }
+  }
+  free(tr.r_debug);
   return 0;
 }
--- mem_strace_hook/track.cc
+++ mem_strace_hook/track.cc
@@ -20,31 +20,31 @@ namespace Track
     s += std::to_string(child) + "/auxv";
     file.open(s.c_str());
     if (file.is_open() == false)
-      {
-        std::cerr << "Couldn't open /proc/" << child << "/auxv\n";
-        return;
-      }
+    {
+      std::cerr << "Couldn't open /proc/" << child << "/auxv\n";
+      return;
+    }
     ElfW(auxv_t) auxv;
     file.read(reinterpret_cast<char *>(&auxv), sizeof (auxv));
     ElfW(auxv_t) *auxv_ptr = &auxv;
     while (auxv_ptr->a_type)
-      {
-        if (auxv_ptr->a_type == AT_PHDR)
-          phdr = reinterpret_cast<ElfW(Phdr) *>(auxv_ptr->a_un.a_val);
-        if (auxv_ptr->a_type == AT_PHNUM)
-          phnum = auxv_ptr->a_un.a_val;
-        if (auxv_ptr->a_type == AT_PHENT)
-          phent = auxv_ptr->a_un.a_val;
-        file.read(reinterpret_cast<char *>(&auxv), sizeof (auxv));
-        if (!file.good())
-          break;
-        auxv_ptr = &auxv;
-      }
+    {
+      if (auxv_ptr->a_type == AT_PHDR)
+        phdr = reinterpret_cast<ElfW(Phdr) *>(auxv_ptr->a_un.a_val);
+      if (auxv_ptr->a_type == AT_PHNUM)
+        phnum = auxv_ptr->a_un.a_val;
+      if (auxv_ptr->a_type == AT_PHENT)
+        phent = auxv_ptr->a_un.a_val;
+      file.read(reinterpret_cast<char *>(&auxv), sizeof (auxv));
+      if (!file.good())
+        break;
+      auxv_ptr = &auxv;
+    }
     if (!phdr || !phnum)
       std::cerr << "No program header found \n";
     file.close();
   }
-
+  
   void tracker::get_r_debug_addr()
   {
     struct r_debug *r_child;
@@ -55,51 +55,51 @@ namespace Track
     local[0].iov_base = buf;
     local[0].iov_len = sizeof (ElfW(Phdr));
     for (unsigned i = 0; i < phnum; i++, ite++)
+    {
+      remote[0].iov_base = ite;
+      remote[0].iov_len = sizeof (ElfW(Phdr));
+      process_vm_readv(child, local, 1, remote, 1, 0);
+      if (reinterpret_cast<ElfW(Phdr) *>(local[0].iov_base)->p_type == PT_DYNAMIC)
       {
-        remote[0].iov_base = ite;
-        remote[0].iov_len = sizeof (ElfW(Phdr));
-        process_vm_readv(child, local, 1, remote, 1, 0);
-        if (reinterpret_cast<ElfW(Phdr) *>(local[0].iov_base)->p_type == PT_DYNAMIC)
-          {
-            ite = reinterpret_cast<ElfW(Phdr) *>(local[0].iov_base);
-            break;
-          }
+        ite = reinterpret_cast<ElfW(Phdr) *>(local[0].iov_base);
+        break;
       }
+    }
     if (ite->p_type != PT_DYNAMIC)
       std::cerr << "No dynamic segment found" << std::endl;
     ElfW(Addr) pt_dynamic = reinterpret_cast<ElfW(Addr)>(ite->p_vaddr);
     local[0].iov_len = sizeof (ElfW(Dyn));
     do
+    {
+      while (true)
       {
-        while (true)
-          {
-            remote[0].iov_base = (void *)pt_dynamic;
-            remote[0].iov_len = sizeof (ElfW(Dyn));
-            process_vm_readv(child, local, 1, remote, 1, 0);
-            if (reinterpret_cast<ElfW(Dyn) *>(local[0].iov_base)->d_tag == DT_DEBUG)
-              {
-                r_child = reinterpret_cast<struct r_debug *>
-                  (reinterpret_cast<ElfW(Dyn) *>(local[0].iov_base)->d_un.d_ptr);
-                break;
-              }
-            if (reinterpret_cast<ElfW(Dyn) *>(local[0].iov_base)->d_tag == DT_NULL)
-              break;
-            pt_dynamic += sizeof (ElfW(Dyn));
-          }
-        if (r_child == 0)
-          {
-            ptrace(PTRACE_SINGLESTEP, child, 0, 0);
-            waitpid(child, 0, 0);
-          }
-        else
-          {
-            Tools::read_from_pid(child, sizeof (struct r_debug), this->r_debug, r_child);
-            break;
-          }
+        remote[0].iov_base = (void *)pt_dynamic;
+        remote[0].iov_len = sizeof (ElfW(Dyn));
+        process_vm_readv(child, local, 1, remote, 1, 0);
+        if (reinterpret_cast<ElfW(Dyn) *>(local[0].iov_base)->d_tag == DT_DEBUG)
+        {
+          r_child = reinterpret_cast<struct r_debug *>
+            (reinterpret_cast<ElfW(Dyn) *>(local[0].iov_base)->d_un.d_ptr);
+          break;
+        }
+        if (reinterpret_cast<ElfW(Dyn) *>(local[0].iov_base)->d_tag == DT_NULL)
+          break;
+        pt_dynamic += sizeof (ElfW(Dyn));
       }
+      if (r_child == 0)
+      {
+        ptrace(PTRACE_SINGLESTEP, child, 0, 0);
+        waitpid(child, 0, 0);
+      }
+      else
+      {
+        Tools::read_from_pid(child, sizeof (struct r_debug), this->r_debug, r_child);
+        break;
+      }
+    }
     while (true);
   }
-
+  
   int tracker::init_tracker()
   {
     get_phdr();
